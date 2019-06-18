0507761f2e26e2e44e7ebfd52bfd91745f11c709
droz <rozhkov@iskrauraltel.ru>
2019-06-18 11:07:14 +0500

    Define PlainRecSessData

--- CompositeKeyIndexTests.cpp
+++ CompositeKeyIndexTests.cpp
@@ -1,6 +1,8 @@
 #include <boost/multi_index_container.hpp>
 #include <boost/multi_index/member.hpp>
 #include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/composite_key.hpp>
+
 #include <boost/multi_index/mem_fun.hpp>
 
 #include "gtest/gtest.h"
@@ -10,8 +12,6 @@ struct HostAndPort
   std::string _host;
   int _port;
 };
-
-
 
 struct PlainRecSessData
 {
@@ -23,4 +23,29 @@ struct PlainRecSessData
   std::string _httpStorageURL;
   HostAndPort _rtpDstA;
   HostAndPort _rtpDstB;
+  struct recSessKey {};
 };
+
+using namespace boost::multi_index;
+
+// Определение типа контейнера, пожалуй, самая запутанная часть работы с multi_index
+typedef multi_index_container<
+  PlainRecSessData, // 1. Тип хранимаого объекта
+  indexed_by        // 2. Блок определения индексов ordered, hashed, ranked, random-access
+  <
+    ordered_unique
+    <
+      // tag - может отсутствовать, тогда обращение к индексу будет производиться по его номеру. В роли tag  используется класс-пустышка
+      // member - ссылка на член класса RecSessData типа RecSessKey
+      tag<PlainRecSessData::recSessKey>,
+      composite_key
+      <
+        PlainRecSessData,
+        member<PlainRecSessData, std::string, &PlainRecSessData::_clientName>,
+        member<PlainRecSessData, int, &PlainRecSessData::_recSessId>
+      >
+    >
+  >
+> RecSessRelations;
+
+
